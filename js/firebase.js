f443cc4a3fc3f0a171811cfa8997b83ace4da95c
davorradic <44604843+davorradic@users.noreply.github.com>
2018-12-16 15:09:38 +0100

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -50,14 +50,6 @@ $(function(){
   
 });
 
-function login(){
-  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ...
-  });
-}
  
 var messagesRef = firebase.database().ref('signups');
 
